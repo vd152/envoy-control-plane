22048f6811782da83a13ccb754badd3d31542a80
Vidhi <vidhi152@gmail.com>
2022-03-30 14:06:11 +0530

    tapping

--- temp/temp.go
+++ temp/temp.go
@@ -15,6 +15,7 @@ func dump_event(direction string, timestamp time.Time, data []byte) string {
 	dump := New()
 	dump.WriteString(direction)
 	dump.WriteString(timestamp.Format("2006-01-02 15:04:05"))
+
 	return dump.GetValueString()
 }
 func main() {
